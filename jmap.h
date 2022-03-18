0bfd995e223235a78ceb4d17f76acb2c526d45f1
donmaks <m1a9k9s6@gmail.com>
2022-03-18 09:18:37 +0100

    Fixed jmap class

--- jmap.h
+++ jmap.h
@@ -83,7 +83,7 @@ namespace jutils
 
         bool contains(const key_type& key) const { return container.find(key) != nullptr; }
 
-        void reserve(const index_type capacity) { container.resizeTable(capacity); }
+        void reserve(const index_type capacity) { container.reserve(capacity); }
 
         template<typename... Args>
         value_type& put(const key_type& key, Args&&... args) { return container.set(key, key, std::forward<Args>(args)...).value; }
